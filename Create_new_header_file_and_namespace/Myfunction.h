2c956bdebcd7cd842513bca3012dcccc2d5116ac
Arshadul <arshadulakib@gmail.com>
2026-05-21 08:37:15 +0600

    Add files via upload

---
+++ Create_new_header_file_and_namespace/Myfunction.h
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+namespace abdullah {
+    void greet() {
+        cout << "Hello from Abdullah's namespace!" << endl;
+    }
+}
---
+++ Create_new_header_file_and_namespace/main_C++_code.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#include "Myfunction.h"
+using namespace std;
+
+using namespace abdullah;
+
+int main() {
+    greet(); // Calling the function from the namespace
+    return 0;
+}
