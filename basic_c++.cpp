853444b3b921a21018fc05448ba32f28248a3550
Arshadul <arshadulakib@gmail.com>
2026-05-24 23:55:47 +0600

    Add files via upload

---
+++ basic_c++.cpp
@@ -0,0 +1,16 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int i, j;
+    double a;
+    i = 22;
+    j = 1;
+    a = 99.021;
+
+    cout << "Here are some values:" << i << ", " << j <<", "<< a << endl;
+
+
+    return 0;
+}
